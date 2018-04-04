2750e719e52c887cebd805df835b667aa8c77614
mnasafi <mnasafi@mail.csuchico.edu>
2018-04-04 15:51:50 -0700

    work in progress... continue Friday

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main()
 {
   int letters;
 
+  vector <vector <char> > alphabet;
+
+
   do
   {
     cout<<"How many letters should there be per line? ";
@@ -17,6 +20,34 @@ int main()
       cout<<"\nPlease enter a number between 1-26.\n";
     }
   }while( letters < 1 || letters > 26 );
+
+
+  int columns;
+  
+  if( 26 % letters == 0 )
+      {
+          columns = 26 / letters;
+
+      } // end for
+
+  else 
+  {
+      columns = 26/lettets + 1;
+
+
+  } 
+
+  // resize grid to have _letter_ # of _rows_, and _columns_ # of columns
+  alphabet.resize( letters );
+
+  for( int row=0; row < alphabet.size(); row++)
+  {
+      grid[row].resize(columns);
+
+  } // end for
+
+
+  // traverse the matrix and assign it incrementing letters from A to Z
 
   return 0;
 }
